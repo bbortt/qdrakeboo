3bce5a05aa04466a4a4284afb7ed8dcbc6cfb05d
bbortt <timon.borter@gmx.ch>
2019-01-23 21:14:57 +0100

    introducing redux store (next.js example from github :)

--- parent/core/web-ui/pages/index.js
+++ parent/core/web-ui/pages/index.js
@@ -1,68 +1,30 @@
-import Layout from '../components/MyLayout.js'
-import Link from 'next/link'
+import React from 'react'
+import {connect} from 'react-redux'
+import {serverRenderClock, startClock} from '../store'
+import Examples from '../components/examples'
 
-function getPosts() {
-  return [
-    {id: 'hello-nextjs', title: 'Hello Next.js'},
-    {id: 'learn-nextjs', title: 'Learn Next.js is awesome'},
-    {id: 'deploy-nextjs', title: 'Deploy apps with ZEIT'},
-  ]
+class Index extends React.Component {
+  static getInitialProps({reduxStore, req}) {
+    const isServer = !!req
+    reduxStore.dispatch(serverRenderClock(isServer))
+
+    return {}
+  }
+
+  componentDidMount() {
+    const {dispatch} = this.props
+    this.timer = startClock(dispatch)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer)
+  }
+
+  render() {
+    return (
+      <Examples/>
+    )
+  }
 }
 
-const PostLink = ({post}) => (
-  <li>
-    <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
-      <a>{post.title}</a>
-    </Link>
-    <style jsx>{`
-      li {
-        list-style: none;
-        margin: 5px 0;
-      }
-
-      a {
-        text-decoration: none;
-        color: blue;
-        font-family: "Arial";
-      }
-
-      a:hover {
-        opacity: 0.6;
-      }
-    `}</style>
-  </li>
-)
-
-export default () => (
-  <Layout>
-    <h1>My Blog</h1>
-    <ul>
-      {getPosts().map((post) => (
-        <PostLink key={post.id} post={post}/>
-      ))}
-    </ul>
-    <style jsx>{`
-      h1, a {
-        font-family: "Arial";
-      }
-
-      ul {
-        padding: 0;
-      }
-
-      li {
-        list-style: none;
-        margin: 5px 0;
-      }
-
-      a {
-        text-decoration: none;
-        color: blue;
-      }
-
-      a:hover {
-        opacity: 0.6;
-      }
-    `}</style>
-  </Layout>
-)
+export default connect()(Index)
