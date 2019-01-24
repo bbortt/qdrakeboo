d5a5b3d939aa7b6ba96625f8f9593dffc96f5424
bbortt <timon.borter@gmx.ch>
2019-01-24 17:43:06 +0100

    remove abstract test?

--- parent/core/authorization-server/src/test/java/io/github/bbortt/tv/core/authorizationserver/AuthorizationServerIntTest.java
+++ parent/core/authorization-server/src/test/java/io/github/bbortt/tv/core/authorizationserver/AuthorizationServerIntTest.java
@@ -1,9 +1,13 @@
 package io.github.bbortt.tv.core.authorizationserver;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-public class AuthorizationServerIntTest
-    extends AbstractAuthorizationServerContextAwareTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = {AuthorizationServer.class})
+public class AuthorizationServerIntTest {
 
   @Test
   public void contextLoads() {
