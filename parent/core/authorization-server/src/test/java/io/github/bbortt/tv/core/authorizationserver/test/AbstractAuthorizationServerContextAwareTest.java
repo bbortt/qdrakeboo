0116774a0f989e62360104e5360a04e3b99b30cf
bbortt <timon.borter@gmx.ch>
2019-01-24 17:57:53 +0100

    does it help to put it into a test package?

--- parent/core/authorization-server/src/test/java/io/github/bbortt/tv/core/authorizationserver/test/AbstractAuthorizationServerContextAwareTest.java
+++ parent/core/authorization-server/src/test/java/io/github/bbortt/tv/core/authorizationserver/test/AbstractAuthorizationServerContextAwareTest.java
@@ -1,16 +1,12 @@
-package io.github.bbortt.tv.core.authorizationserver;
+package io.github.bbortt.tv.core.authorizationserver.test;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import io.github.bbortt.tv.core.authorizationserver.AuthorizationServer;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {AuthorizationServer.class})
-public class AuthorizationServerIntTest {
+public abstract class AbstractAuthorizationServerContextAwareTest {
 
-  @Test
-  public void contextLoads() {
-
-  }
 }
---
+++ parent/core/authorization-server/src/test/java/io/github/bbortt/tv/core/authorizationserver/test/AuthorizationServerIntTest.java
@@ -0,0 +1,12 @@
+package io.github.bbortt.tv.core.authorizationserver.test;
+
+import org.junit.Test;
+
+
+public class AuthorizationServerIntTest extends AbstractAuthorizationServerContextAwareTest {
+
+  @Test
+  public void contextLoads() {
+
+  }
+}
