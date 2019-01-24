064dae04a573017114bf85cdf88b618bdf04ffed
bbortt <timon.borter@gmx.ch>
2019-01-24 17:21:18 +0100

    remove context configuration.

--- parent/core/authorization-server/src/test/java/io/github/bbortt/tv/core/authorizationserver/AbstractAuthorizationServerContextAwareTest.java
+++ parent/core/authorization-server/src/test/java/io/github/bbortt/tv/core/authorizationserver/AbstractAuthorizationServerContextAwareTest.java
@@ -3,10 +3,8 @@ package io.github.bbortt.tv.core.authorizationserver;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@ContextConfiguration
 @AutoConfigureMockMvc
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {AuthorizationServer.class})
