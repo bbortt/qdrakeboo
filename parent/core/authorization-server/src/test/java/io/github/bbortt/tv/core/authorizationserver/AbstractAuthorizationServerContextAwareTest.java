20d48beaf0876bc26d639db0be7043d76bf4b122
bbortt <timon.borter@gmx.ch>
2019-01-26 13:21:20 +0100

    fix integration test. problem was spring jar structure.

--- parent/core/authorization-server/src/test/java/io/github/bbortt/tv/core/authorizationserver/AbstractAuthorizationServerContextAwareTest.java
+++ parent/core/authorization-server/src/test/java/io/github/bbortt/tv/core/authorizationserver/AbstractAuthorizationServerContextAwareTest.java
@@ -2,10 +2,11 @@ package io.github.bbortt.tv.core.authorizationserver;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {AuthorizationServer.class})
+@SpringBootTest(classes = {AuthorizationServer.class}, webEnvironment = WebEnvironment.RANDOM_PORT)
 public abstract class AbstractAuthorizationServerContextAwareTest {
 
 }
--- parent/core/authorization-server/src/test/java/io/github/bbortt/tv/core/authorizationserver/AuthorizationServerIntTest.java
+++ parent/core/authorization-server/src/test/java/io/github/bbortt/tv/core/authorizationserver/AuthorizationServerIntTest.java
@@ -2,7 +2,6 @@ package io.github.bbortt.tv.core.authorizationserver;
 
 import org.junit.Test;
 
-
 public class AuthorizationServerIntTest extends AbstractAuthorizationServerContextAwareTest {
 
   @Test
