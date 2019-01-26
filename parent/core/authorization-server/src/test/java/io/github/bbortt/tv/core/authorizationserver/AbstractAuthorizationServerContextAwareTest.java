3944058ea425eb769a78952464f70871e31286a1
bbortt <timon.borter@gmx.ch>
2019-01-26 08:32:45 +0100

    add spring boot test classes.

--- parent/core/authorization-server/src/test/java/io/github/bbortt/tv/core/authorizationserver/AbstractAuthorizationServerContextAwareTest.java
+++ parent/core/authorization-server/src/test/java/io/github/bbortt/tv/core/authorizationserver/AbstractAuthorizationServerContextAwareTest.java
@@ -4,8 +4,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
 @RunWith(SpringRunner.class)
+@SpringBootTest(classes = {AuthorizationServer.class})
 public abstract class AbstractAuthorizationServerContextAwareTest {
 
 }
