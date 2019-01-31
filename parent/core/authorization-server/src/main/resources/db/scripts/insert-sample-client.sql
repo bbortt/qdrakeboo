bf4cfd53adefe60bd915f15d1ed2fe95b6002188
bbortt <timon.borter@gmx.ch>
2019-01-31 17:38:29 +0100

    starting local development.

--- parent/core/authorization-server/src/main/resources/db/scripts/insert-sample-client.sql
+++ parent/core/authorization-server/src/main/resources/db/scripts/insert-sample-client.sql
@@ -2,29 +2,29 @@ INSERT INTO client
     (client_id, secret, access_token_validity, refresh_token_validity, redirect_uris)
   VALUES
     (
-      'cea19e6f-fa31-407f-a05a-a20d28c4c74d',
-      crypt('8779af4a-d120-4bea-9993-dea2fbc4b571', gen_salt('bf')),
+      'deea2da1-21a6-492c-a0f3-a5e9cd6301e2',
+      crypt('54e76b2f-fc53-4ec2-aee5-8d9df2fff625', gen_salt('bf')),
       3600,
       7200,
-      'http://localhost:3000'
+      'http://localhost:8080/user'
     );
 
 INSERT INTO client_has_grant_types
     (client_id, grant_type_id)
   VALUES
-    (1, 1), (1, 4);
+    (2, 1);
 
 INSERT INTO authority
     (name)
   VALUES
-    ('ui');
+    ('user');
 
 INSERT INTO client_has_authorities
     (client_id, authority_id)
   VALUES
-    (1, 1);
+    (2, 2);
 
 INSERT INTO client_has_scopes
     (client_id, scope_id)
   VALUES
-    (1, 1);
+    (2, 1), (2, 2), (2, 3);
