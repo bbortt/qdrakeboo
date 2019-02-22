3380b3c65ae82ae5e98e23ece3a03add797309e7
bbortt <timon.borter@gmx.ch>
2019-02-22 13:22:51 +0100

    move oauth2 client configuration to separate module.

---
+++ parent/lib/oauth2-client-autoconfiguration/src/main/java/io/github/bbortt/tv/lib/oauth2/client/autoconfiguration/config/CorsFilterConfig.java
@@ -0,0 +1,18 @@
+package io.github.bbortt.tv.lib.oauth2.client.autoconfiguration.config;
+
+import javax.servlet.Filter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import io.github.bbortt.tv.lib.oauth2.client.autoconfiguration.filter.CorsFilter;
+
+@Configuration
+public class CorsFilterConfig {
+
+  @Bean
+  @Order(Ordered.HIGHEST_PRECEDENCE)
+  public Filter corsFilter() {
+    return new CorsFilter();
+  }
+}
---
+++ parent/lib/oauth2-client-autoconfiguration/src/main/java/io/github/bbortt/tv/lib/oauth2/client/autoconfiguration/config/OAuth2ClientAutoconfigurationProperties.java
@@ -0,0 +1,67 @@
+package io.github.bbortt.tv.lib.oauth2.client.autoconfiguration.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "oauth2.client")
+public class OAuth2ClientAutoconfigurationProperties {
+
+  private String clientId;
+  private String clientSecret;
+  private String resourceId;
+  private String checkTokenUri;
+
+  private Cors cors;
+
+  public String getClientId() {
+    return clientId;
+  }
+
+  public void setClientId(String clientId) {
+    this.clientId = clientId;
+  }
+
+  public String getClientSecret() {
+    return clientSecret;
+  }
+
+  public void setClientSecret(String clientSecret) {
+    this.clientSecret = clientSecret;
+  }
+
+  public String getResourceId() {
+    return resourceId;
+  }
+
+  public void setResourceId(String resourceId) {
+    this.resourceId = resourceId;
+  }
+
+  public String getCheckTokenUri() {
+    return checkTokenUri;
+  }
+
+  public void setCheckTokenUri(String checkTokenUri) {
+    this.checkTokenUri = checkTokenUri;
+  }
+
+  public Cors getCors() {
+    return cors;
+  }
+
+  public void setCors(Cors cors) {
+    this.cors = cors;
+  }
+
+  public static class Cors {
+
+    private boolean enabled;
+
+    public boolean isEnabled() {
+      return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+      this.enabled = enabled;
+    }
+  }
+}
