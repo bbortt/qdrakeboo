99492233884408d5bff6da855e38b38e483d1767
bbortt <timon.borter@gmx.ch>
2019-05-03 08:32:46 +0200

    redirect unauthenticated to session endpoint.

--- parent/frontend/web-ui/pages/index.js
+++ parent/frontend/web-ui/pages/index.js
@@ -8,27 +8,35 @@ require('./index.scss')
 
 class Index extends React.Component<Index.propTypes> {
 
+  static async getInitialProps({ctx}) {
+    const {query} = ctx
+
+    const isAuthenticated = query.isAuthenticated
+
+    return {isAuthenticated}
+  }
+
   launch = () => {
-    Router.push('/home')
+    Router.push('/session')
   }
 
   render() {
     return (
-        <div className='Index'>
-          <div className='grid-container'>
-            <div className='grid-x'>
-              <div className='cell'>
-                <h1>Welcome, stranger!</h1>
-              </div>
+      <div className='Index'>
+        <div className='grid-container'>
+          <div className='grid-x'>
+            <div className='cell'>
+              <h1>Welcome, stranger!</h1>
+            </div>
 
-              <div className='cell'>
-                <button className='button'
-                        onClick={this.launch}>{this.props.isAuthenticated
-                    ? 'Launch' : 'Sign In'}</button>
-              </div>
+            <div className='cell'>
+              <button className='button'
+                      onClick={this.launch}>{this.props.isAuthenticated
+                ? 'Launch' : 'Sign In'}</button>
             </div>
           </div>
         </div>
+      </div>
     )
   }
 }
