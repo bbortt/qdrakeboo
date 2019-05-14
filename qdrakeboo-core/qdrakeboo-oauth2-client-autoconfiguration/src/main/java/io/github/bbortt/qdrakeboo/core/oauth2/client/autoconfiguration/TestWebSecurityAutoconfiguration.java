0eae81b147f2d3d87dc0570292f96d51a4e350db
Timon Borter <timon.borter@gmx.ch>
2019-05-14 11:27:05 +0200

    copy graphql starter test.

--- qdrakeboo-core/qdrakeboo-oauth2-client-autoconfiguration/src/main/java/io/github/bbortt/qdrakeboo/core/oauth2/client/autoconfiguration/TestWebSecurityAutoconfiguration.java
+++ qdrakeboo-core/qdrakeboo-oauth2-client-autoconfiguration/src/main/java/io/github/bbortt/qdrakeboo/core/oauth2/client/autoconfiguration/TestWebSecurityAutoconfiguration.java
@@ -1,7 +1,7 @@
 package io.github.bbortt.qdrakeboo.core.oauth2.client.autoconfiguration;
 
 import io.github.bbortt.qdrakeboo.core.oauth2.client.autoconfiguration.config.OAuth2ClientAutoconfigurationProperties;
-import io.github.bbortt.qdrakeboo.core.oauth2.client.autoconfiguration.config.WebSecurityConfig;
+import io.github.bbortt.qdrakeboo.core.oauth2.client.autoconfiguration.config.TestWebSecurityConfig;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @ConditionalOnProperty(prefix = "oauth2.client.autoconfiguration", name = "enabled",
     havingValue = "false")
-@Import({WebSecurityConfig.class})
+@Import({TestWebSecurityConfig.class})
 @EnableConfigurationProperties({OAuth2ClientAutoconfigurationProperties.class})
 public class TestWebSecurityAutoconfiguration {
 
