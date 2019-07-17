1e324fb76c0fea103ac239fe35110d51e99a873a
Timon Borter <timon.borter@gmx.ch>
2019-07-17 22:06:10 +0200

    trying to load route configuration from variable file - needs some testing!

--- source/edge-gateway/src/main/java/io/github/bbortt/qdrakeboo/edgegateway/configuration/RouteConfiguration.java
+++ source/edge-gateway/src/main/java/io/github/bbortt/qdrakeboo/edgegateway/configuration/RouteConfiguration.java
@@ -1,12 +1,10 @@
 package io.github.bbortt.qdrakeboo.edgegateway.configuration;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@Profile({"!dev & !test"})
-@PropertySource({"config/route.properties"})
+@PropertySource({"${routes.path}"})
 public class RouteConfiguration {
 
 }
