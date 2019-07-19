078c27ba9c0130459e523c89abe7cae214a8f6a9
Timon Borter <timon.borter@gmx.ch>
2019-07-19 08:00:25 +0200

    dockerize all builds and add version to travis-ci.

---
+++ scripts/docker_build_all.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# Builds all Docker images. Caution: Depends on `./gradlew build`!
+# Must be started from within the root directory!
+#
+# Usage: `./docker_build_all.sh <build-version>`
+
+if [[ -z $1 ]] ; then
+  echo -e "Usage: \`$ ./docker_build_all.sh <build-version>\`
+
+  \t<build-version>\tThe build output version."
+  exit 1
+fi
+
+docker build \
+  --build-arg JAR_FILE=edge-gateway-$1.jar \
+  -t qdrakeboo/edge-gateway:$1 \
+  -f source/edge-gateway/Dockerfile .
+
+docker build \
+  --build-arg JAR_FILE=upstream-server-$1.jar \
+  -t qdrakeboo/upstream-server:$1 \
+  -f source/stream/upstream-server/Dockerfile .
+
+docker build \
+  -t qdrakeboo/apollo-federation-express-gateway:$1 \
+  -f source/federation/apollo-federation-express-gateway/Dockerfile .
+
+docker build \
+  -t qdrakeboo/next-js-web-ui:$1 \
+  -f source/frontend/next-js-web-ui/Dockerfile .
