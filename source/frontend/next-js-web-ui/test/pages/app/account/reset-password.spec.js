b5efd93c7131b747742e7a25d880e643c3497323
Timon Borter <timon.borter@gmx.ch>
2019-09-19 20:14:08 +0200

    fix frontend test with store prop.

--- source/frontend/next-js-web-ui/test/pages/app/account/reset-password.spec.js
+++ source/frontend/next-js-web-ui/test/pages/app/account/reset-password.spec.js
@@ -7,7 +7,9 @@ import { expect } from 'chai'
 import { ResetPasswordClass } from '../../../../pages/app/account/reset-password'
 
 describe('ResetPassword', () => {
-  const resetPassword = shallow(<ResetPasswordClass />)
+  const resetPassword = shallow(
+    <ResetPasswordClass store={{ subscribe: () => {} }} />
+  )
 
   it('is DIVided', () => {
     expect(resetPassword.find('.reset-password')).to.be.an('object')
