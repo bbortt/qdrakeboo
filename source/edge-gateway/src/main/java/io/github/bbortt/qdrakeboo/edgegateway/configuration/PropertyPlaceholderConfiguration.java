b78886f66e954eb3462d4b452373400438dc18fd
Travis CI <builds@travis-ci.org>
2019-10-18 13:47:47 +0200

    map secret to env instead of file.

--- source/edge-gateway/src/main/java/io/github/bbortt/qdrakeboo/edgegateway/configuration/PropertyPlaceholderConfiguration.java
+++ source/edge-gateway/src/main/java/io/github/bbortt/qdrakeboo/edgegateway/configuration/PropertyPlaceholderConfiguration.java
@@ -1,34 +1,10 @@
 package io.github.bbortt.qdrakeboo.edgegateway.configuration;
 
-import java.io.IOException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @Configuration
 public class PropertyPlaceholderConfiguration {
-
-  private static final Logger LOGGER = LoggerFactory
-      .getLogger(PropertyPlaceholderConfiguration.class);
-
-  @Bean
-  @ConditionalOnProperty(name = "${AUTH0_PROPERTIES_PLACEHOLDER}")
-  public PropertySourcesPlaceholderConfigurer placeHolderConfigurer(
-      @Value("${AUTH0_PROPERTIES_PLACEHOLDER}") String auth0PropertiesPlaceholder)
-      throws IOException {
-    LOGGER.info("Initializing property source from '{}'", auth0PropertiesPlaceholder);
-
-    PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-    propertySourcesPlaceholderConfigurer.setLocations(
-        new PathMatchingResourcePatternResolver().getResources(auth0PropertiesPlaceholder));
-    return propertySourcesPlaceholderConfigurer;
-  }
 
   @Configuration
   @ConfigurationProperties("auth0")
