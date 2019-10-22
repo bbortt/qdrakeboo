b2410582196c1e8e267da641533003a686f18671
Timon Borter <timon.borter@gmx.ch>
2019-10-22 16:53:41 +0200

    make redis password configurable.

--- source/frontend/next-js-web-ui/server.js
+++ source/frontend/next-js-web-ui/server.js
@@ -30,6 +30,7 @@ if (!dev) {
   const redis = require('redis').createClient({
     host: process.env.REDIS_HOST || '127.0.0.1',
     port: process.env.REDIS_PORT || 6379,
+    password: process.env.REDIS_PASSWORD,
   })
   const RedisStore = require('connect-redis')(session)
   sessionConfig.store = new RedisStore({ client: redis })
