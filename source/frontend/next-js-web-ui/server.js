30d2873df46805c1d998b5fefa5958a2c6f3f33e
Timon Borter <timon.borter@gmx.ch>
2019-10-23 19:52:27 +0200

    make redis protocol and db configurable.

--- source/frontend/next-js-web-ui/server.js
+++ source/frontend/next-js-web-ui/server.js
@@ -28,7 +28,11 @@ if (!dev) {
   sessionConfig.cookie.secure = true
 
   const redis = require('redis').createClient({
-    url: `redis://${process.env.REDIS_USER}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+    url: `${process.env.REDIS_PROTOCOL || 'redis'}://${
+      process.env.REDIS_USER
+    }:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${
+      process.env.REDIS_PORT
+    }/${process.env.REDIS_DB || ''}`,
   })
   const RedisStore = require('connect-redis')(session)
   sessionConfig.store = new RedisStore({ client: redis })
